c05e7d8661efe27400be0e43c78567d4d6e2c1cf
Alexander Kazantsev <akazantsev@s-pro.io>
2018-03-18 15:45:06 +0200

    added basic usages

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-function* makeGenerator() {
-  yield 1;
-  yield 2;
-  yield 'foo';
-  return 'bar';
-}
+const str = 'abc';
+const arr = ['a', 'b', 'c'];
+const typedArr = new Int16Array();
+const map = new Map();
+const set = new Set();
+const obj = new Object();
 
-const generator = makeGenerator();
+console.log(str[Symbol.iterator]);
+console.log(arr[Symbol.iterator]);
+console.log(typedArr[Symbol.iterator]);
+console.log(typedArr[Symbol.iterator]);
+console.log(map[Symbol.iterator]);
+console.log(set[Symbol.iterator]);
 
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next()); // done
-console.log(generator.next()); 
+console.log(obj[Symbol.iterator]); // undefined
 
+try {
+  for (let v of anotherObj) console.log(v); // throws an error
+} catch (e) { console.log('ERROR'); }
+
+const anotherObj = {
+  [Symbol.iterator]: function* () { },
+  foo: 1,
+  bar: 2.14
+};
+
+for (let v of anotherObj) console.log(v);
