dbea0ac40cfc98ca7d75341495b9d42957ed8b73
Alexander Kazantsev <akazantsev@s-pro.io>
2018-03-21 13:59:20 +0200

    added throw example

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const str = 'abc';
-const arr = ['a', 'b', 'c'];
-const typedArr = new Int16Array();
-const map = new Map();
-const set = new Set();
-const obj = new Object();
+function* gen() {
+  try {
+  const error = yield 9;
+  } catch(e) {
+    console.log('ERROR ->> ', e);
+  }
+}
 
-console.log(str[Symbol.iterator]);
-console.log(arr[Symbol.iterator]);
-console.log(typedArr[Symbol.iterator]);
-console.log(typedArr[Symbol.iterator]);
-console.log(map[Symbol.iterator]);
-console.log(set[Symbol.iterator]);
+const _gen = gen();
+console.log(_gen.next());
 
-console.log(obj[Symbol.iterator]); // undefined
-
-try {
-  for (let v of anotherObj) console.log(v); // throws an error
-} catch (e) { console.log('ERROR'); }
-
-const anotherObj = {
-  [Symbol.iterator]: function* () { },
-  foo: 1,
-  bar: 2.14
-};
-
-for (let v of anotherObj) console.log(v);
+_gen.throw('SOME ERROR');
