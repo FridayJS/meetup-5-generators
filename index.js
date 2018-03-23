efd9774307c11008f344b0a4d894e3d87e561c9b
Alexander Kazantsev <akazantsev@s-pro.io>
2018-03-23 11:56:11 +0200

    added basic generator

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const str = 'abc';
-const arr = ['a', 'b', 'c'];
-const typedArr = new Int16Array();
-const map = new Map();
-const set = new Set(['a', 'b', 'c']);
-const obj = new Object();
+/** GENERATORS */
 
-console.log(str[Symbol.iterator]);
-console.log(arr[Symbol.iterator]);
-console.log(typedArr[Symbol.iterator]);
-console.log(map[Symbol.iterator]);
-console.log(set[Symbol.iterator]);
-
-console.log(obj[Symbol.iterator]); // undefined
+function* makeGenerator() {
+  yield 'foo';
+}
