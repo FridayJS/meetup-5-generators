77d568e8eefc738e18b47d0f72c94a62c3740655
Alexander Kazantsev <akazantsev@s-pro.io>
2018-03-23 10:28:46 +0200

    cleared code

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const obj = new Object();
 console.log(str[Symbol.iterator]);
 console.log(arr[Symbol.iterator]);
 console.log(typedArr[Symbol.iterator]);
-console.log(typedArr[Symbol.iterator]);
 console.log(map[Symbol.iterator]);
 console.log(set[Symbol.iterator]);
 
 console.log(obj[Symbol.iterator]); // undefined
-
-try {
-  for (let v of anotherObj) console.log(v); // throws an error
-} catch (e) { console.log('ERROR'); }
-
